a7799ac8c10d03db0a499c74d88bce4028ce9e29
unknown <praneshck7@gmail.com>
2024-11-04 16:09:31 +0600

    favourites issue fixed

--- lib/screen/favourite.dart
+++ lib/screen/favourite.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:recepie_app/provider/favourites_provider.dart';
+import 'package:recepie_app/widgets/recepie_details.dart';
 
 class Favourite extends ConsumerStatefulWidget {
   const Favourite({super.key});
@@ -52,75 +53,86 @@ class FavouriteState extends ConsumerState<Favourite> {
                   background: Container(
                     color: Colors.red, // Background color when swiped
                   ),
-                  child: Card(
-                    margin: const EdgeInsets.symmetric(
-                        horizontal: 16, vertical: 10),
-                    elevation: 4,
-                    child: SizedBox(
-                      height: 150,
-                      child: Padding(
-                        padding: const EdgeInsets.all(1.0),
-                        child: Row(
-                          children: [
-                            // Image on the left side
-                            Flexible(
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(1),
-                                child: Image.network(
-                                  recipe.image,
-                                  width: 150,
-                                  fit: BoxFit.cover,
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => RecipeDetailScreen(
+                            id: index + 1,
+                          ),
+                        ),
+                      );
+                    },
+                    child: Card(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 10),
+                      elevation: 4,
+                      child: SizedBox(
+                        height: 150,
+                        child: Padding(
+                          padding: const EdgeInsets.all(1.0),
+                          child: Row(
+                            children: [
+                              // Image on the left side
+                              Flexible(
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(1),
+                                  child: Image.network(
+                                    recipe.image,
+                                    width: 150,
+                                    fit: BoxFit.cover,
+                                  ),
                                 ),
                               ),
-                            ),
-                            const SizedBox(width: 16),
-                            // Details (name, cooking time, and calories) on the right side
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    recipe.name,
-                                    style: const TextStyle(
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                  const SizedBox(height: 8),
-                                  Row(
-                                    children: [
-                                      const Icon(
-                                        Icons.timelapse_outlined,
-                                        color: Colors.blue,
-                                      ),
-                                      const SizedBox(
-                                        width: 6,
-                                      ),
-                                      Text(
-                                        "${recipe.cookTimeMinutes + recipe.prepTimeMinutes} minutes",
-                                        style: const TextStyle(fontSize: 16),
-                                      ),
-                                    ],
-                                  ),
-                                  const SizedBox(height: 4),
-                                  Row(
-                                    children: [
-                                      const Icon(
-                                        Icons.local_fire_department,
-                                        color: Colors.red,
-                                      ),
-                                      const SizedBox(
-                                        width: 6,
-                                      ),
-                                      Text(
-                                        "${recipe.caloriesPerServing} cal",
-                                        style: const TextStyle(fontSize: 16),
-                                      ),
-                                    ],
-                                  )
-                                ],
+                              const SizedBox(width: 16),
+                              // Details (name, cooking time, and calories) on the right side
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      recipe.name,
+                                      style: const TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    const SizedBox(height: 8),
+                                    Row(
+                                      children: [
+                                        const Icon(
+                                          Icons.timelapse_outlined,
+                                          color: Colors.blue,
+                                        ),
+                                        const SizedBox(
+                                          width: 6,
+                                        ),
+                                        Text(
+                                          "${recipe.cookTimeMinutes + recipe.prepTimeMinutes} minutes",
+                                          style: const TextStyle(fontSize: 16),
+                                        ),
+                                      ],
+                                    ),
+                                    const SizedBox(height: 4),
+                                    Row(
+                                      children: [
+                                        const Icon(
+                                          Icons.local_fire_department,
+                                          color: Colors.red,
+                                        ),
+                                        const SizedBox(
+                                          width: 6,
+                                        ),
+                                        Text(
+                                          "${recipe.caloriesPerServing} cal",
+                                          style: const TextStyle(fontSize: 16),
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
                     ),
