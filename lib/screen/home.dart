b7d22e031a30519269995721c2cfcf8a4129c162
unknown <praneshck7@gmail.com>
2024-11-04 14:53:26 +0600

    first phase done

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:recepie_app/Model/recipe.dart';
 import 'package:recepie_app/constant/api_endpoint.dart';
+import 'package:recepie_app/constant/country.dart';
 import 'package:recepie_app/provider/recipe_provider.dart';
 import 'package:recepie_app/widgets/recepie_details.dart';
 import 'package:recepie_app/widgets/recipe_card.dart';
@@ -18,6 +19,7 @@ class Home extends ConsumerStatefulWidget {
 class HomeState extends ConsumerState<Home> {
   String searchItem = "";
   bool isLoading = true;
+  List<String> cuisines = Country.cuisines;
 
   @override
   void initState() {
@@ -30,19 +32,17 @@ class HomeState extends ConsumerState<Home> {
       final response = await http.get(Uri.parse(ApiEndpoint.baseUrl));
 
       if (response.statusCode == 200) {
-        setState(() {
-          List<Recipe> recipes = (jsonDecode(response.body)['recipes'] as List)
-              .map((recipeData) => Recipe.fromJson(recipeData))
-              .toList();
-          ref.read(recipeProvider.notifier).setRecipe(recipes);
-          isLoading = false;
-        });
+        List<Recipe> recipes = (jsonDecode(response.body)['recipes'] as List)
+            .map((recipeData) => Recipe.fromJson(recipeData))
+            .toList();
+        ref.read(recipeProvider.notifier).setRecipe(recipes);
       } else {
-        setState(() {
-          isLoading = false;
-        });
+        // Log the response in case of failure
+        debugPrint("Failed to fetch recipes: ${response.statusCode}");
       }
     } catch (e) {
+      debugPrint("Error fetching recipes: $e");
+    } finally {
       setState(() {
         isLoading = false;
       });
@@ -65,73 +65,68 @@ class HomeState extends ConsumerState<Home> {
           style: TextStyle(color: Colors.white),
         ),
       ),
-      body:
-          // Loading indicator
-          Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // Filter Chips
-          TextField(
-            onChanged: (value) {
-              setState(() {
-                searchItem = value;
-              });
-            },
-            decoration: const InputDecoration(
-              hintText: 'Search recipes...',
-              suffixIcon: Icon(Icons.search),
-              border: InputBorder.none,
-              filled: true,
-              fillColor: Colors.white,
-              contentPadding:
-                  EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-            ),
-          ),
-
-          // ListView.builder(
-          //     scrollDirection: Axis.horizontal,
-          //     itemBuilder: (context, index) {
-          //       Card(
-          //         child: Text("Whaever"),
-          //       );
-          //     }),
-
-          // Grid View
-          Expanded(
-            child: GridView.builder(
-              padding: const EdgeInsets.all(8),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 8.0,
-                mainAxisSpacing: 8.0,
-                childAspectRatio: 0.90,
-              ),
-              itemCount: filterRecipes.length,
-              itemBuilder: (context, index) {
-                Recipe recipe = filterRecipes[index];
-                return InkWell(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => RecipeDetailScreen(
-                                  id: index + 1,
-                                )));
+      body: isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Search bar
+                TextField(
+                  onChanged: (value) {
+                    setState(() {
+                      searchItem = value;
+                    });
                   },
-                  child: RecipeCard(
-                    id: recipe.id,
-                    imageUrl: recipe.image,
-                    title: recipe.name,
-                    duration:
-                        '${recipe.prepTimeMinutes + recipe.cookTimeMinutes} min',
-                    calories: '${recipe.caloriesPerServing} Cal',
+                  decoration: const InputDecoration(
+                    hintText: 'Search recipes...',
+                    suffixIcon: Icon(Icons.search),
+                    border: InputBorder.none,
+                    filled: true,
+                    fillColor: Colors.white,
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 25, horizontal: 10),
                   ),
-                );
-              },
+                ),
+
+                // Recipe Grid
+                Expanded(
+                  child: GridView.builder(
+                    padding: const EdgeInsets.all(8),
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 8.0,
+                      mainAxisSpacing: 8.0,
+                      childAspectRatio: 0.90,
+                    ),
+                    itemCount: filterRecipes.length,
+                    itemBuilder: (context, index) {
+                      Recipe recipe = filterRecipes[index];
+                      return InkWell(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => RecipeDetailScreen(
+                                id: recipe.id,
+                              ),
+                            ),
+                          );
+                        },
+                        child: RecipeCard(
+                          id: recipe.id,
+                          imageUrl: recipe.image,
+                          title: recipe.name,
+                          duration:
+                              '${recipe.prepTimeMinutes + recipe.cookTimeMinutes} min',
+                          calories: '${recipe.caloriesPerServing} Cal',
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
-      ),
     );
   }
 }
